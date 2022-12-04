cf23167d546a7d909af6dfc2a5c12a5d777b54ca
Zeha Irawan <zehairawan@gmail.com>
2022-12-05 05:20:46 +0800

    Create article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # Combination CRUD of routes is called resources
+  resources :articles
 end
