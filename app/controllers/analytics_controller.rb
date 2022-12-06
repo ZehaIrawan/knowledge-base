4a52df2d803f48c760c0fd8a56aa2b5c67a26fc8
Zeha Irawan <zehairawan@gmail.com>
2022-12-07 07:46:41 +0800

    Display search volume

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,24 @@
 class AnalyticsController < ApplicationController
   def index
     @keywords = Keyword.all
+    @formatted_keywords = Hash.new(0)
+
+# iterate over the array, counting duplicate entries
+
+# print names
+# print hash
+print @formatted_keywords
+print @keywords
+
+@keywords.each { |keyword|
+  print keyword[:query]
+  print "\n"
+  @formatted_keywords[keyword[:query]] += 1
+  @formatted_keywords[:query] = keyword[:query]
+}
+
+print @formatted_keywords
+
+
   end
 end
