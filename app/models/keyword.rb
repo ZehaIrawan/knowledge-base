f2308720792e6f60a90ace7e032c8b947e31b185
Zeha Irawan <zehairawan@gmail.com>
2022-12-07 06:31:49 +0800

    Limit uniqueness scope to user

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,4 @@
 class Keyword < ApplicationRecord
   belongs_to :user
-  validates :query, presence: true, uniqueness: { case_sensitive: false }
+  validates :query, presence: true, uniqueness: { case_sensitive: false,scope: :user_id }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,64 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Keyword.destroy_all
+User.destroy_all
+
+User.create!([
+    {
+  id: 1,
+  email: "zeha@gmail.com",
+  password: "123456"
+},
+  {
+  id: 99,
+  email: "mlarson@mills.info",
+  password: "mlarson@mills.info"
+},
+{
+  id: 100,
+  email: "shanie.armstrong@lubowitz.com",
+  password: "shanie.armstrong@lubowitz.com"
+},
+{
+  id: 101,
+  email: "myah.mcclure@gmail.com",
+  password: "myah.mcclure@gmail.com"
+}
+])
+
+p "Created #{User.count} users"
+
+
+Keyword.create!([
+  {
+    id: 2001,
+    query: "how to create your coaching frameworks",
+    created_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    updated_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    user_id: 99
+},
+  {
+    id: 2002,
+    query: "placing calls in salesforce",
+    created_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    updated_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    user_id: 99
+},
+  {
+    id: 2003,
+    query: "placing calls in salesforce",
+    created_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    updated_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    user_id: 100
+},
+  {
+    id: 2004,
+    query: "how to snippet a call ",
+    created_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    updated_at: 'Tue, 06 Dec 2022 20:48:40.295579000 UTC +00:00',
+    user_id: 100
+}
+])
+
+p "Created #{Keyword.count} keyword query"
